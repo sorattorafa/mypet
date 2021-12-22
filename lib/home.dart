9660ef6a976ab9580ec9be1da30e8664cac3725f
sorattorafa <rampimsoratto@gmail.com>
2021-12-21 22:55:38 -0300

    refact: add gitignore

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+
   final String title;
 
   @override
